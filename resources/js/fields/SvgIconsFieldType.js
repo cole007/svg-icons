636de5fd4e015520dcaa4459cf87a2ca9100d8a0
Kirk Bentley <kirk@fyrebase.com>
2016-11-18 10:25:11 +1000

    [Fixed] Fixed bug when using defs inside sprite sheets not displaying correctly in CP

--- resources/js/fields/SvgIconsFieldType.js
+++ resources/js/fields/SvgIconsFieldType.js
@@ -17,12 +17,39 @@
 		container: null,
 		init: function(options)
 		{
+			var self = this
 			this.options = options;
-
-			this.container = $("#" + options.inputId + '-field')
-
+			this.container = $("#" + this.options.inputId + '-field')
 			this.loadSpriteSheets()
+			this.poll = setInterval(this.selectize.bind(this), 100);
+		},
+		loadSpriteSheets: function() {
+			var self = this;
 
+			for (var i = 0; i < this.options.spriteSheets.length; i++) {
+				var sheet = this.options.spriteSheets[i];
+				if ($.inArray(sheet, __svgicons.loading) == -1) {
+					__svgicons.loading.push(sheet)
+					$.get(sheet, function(data) {
+						var div = document.createElement('div')
+						div.innerHTML = new XMLSerializer().serializeToString(data.documentElement)
+						$svg = $(div).find('> svg')
+						$svg.css('display', 'none').prependTo('body')
+						__svgicons.loaded.push(sheet)
+						__svgicons.loading.splice(__svgicons.loading.indexOf(sheet), 1);
+					});
+				}
+			}
+
+		},
+		selectize: function() {
+			if (__svgicons.loaded.length == this.options.spriteSheets.length) {
+				clearInterval(this.poll)
+				this._selectize();
+			}
+		},
+		_selectize: function() {
+			var self = this;
 			var $sel = this.container.find('.svgicons__select')
 
 			var opt = {
@@ -43,10 +70,10 @@
 							var match = myRegexp.exec(item.value);
 							var c = match[3];
 
-							return '<div class="svgicons__si"><div class="svgicons__si__i"><svg viewBox="0 0 1000 1000"><use xlink:href="#' + c + '" /></svg></div><span>' + escape(item.text) + '</span></div>';
+							return '<div class="svgicons__si"><div class="svgicons__si__i"><svg style="width: 100%; height: 100%;" viewBox="' + $('#' + c)[0].getAttribute('viewBox') +  '"><use xlink:href="#' + c + '" /></svg></div><span>' + escape(item.text) + '</span></div>';
 
 						} else {
-							return '<div class="svgicons__si"><div class="svgicons__si__i"><img src="' + (item.value == '_blank_' ? options.blank : options.iconSetUrl + item.value) + '" alt="" /></div><span>' + escape(item.text) + '</span></div>';
+							return '<div class="svgicons__si"><div class="svgicons__si__i"><img src="' + (item.value == '_blank_' ? self.options.blank : self.options.iconSetUrl + item.value) + '" alt="" /></div><span>' + escape(item.text) + '</span></div>';
 						}
 					},
 					option: function(item, escape) {
@@ -64,32 +91,16 @@
 							var match = myRegexp.exec(item.value);
 							var c = match[3];
 
-							return '<div class="svgicons__i"><div class="svgicons__i__w"><div class="svgicons__i__w__i"><svg viewBox="0 0 1000 1000"><use xlink:href="#' + c + '" /></svg></div></div><span>' + escape(item.text) + '</span></div>';
+							return '<div class="svgicons__i"><div class="svgicons__i__w"><div class="svgicons__i__w__i"><svg style="width: 100%; height: 100%;" viewBox="' + $('#' + c)[0].getAttribute('viewBox') +  '"><use xlink:href="#' + c + '" /></svg></div></div><span>' + escape(item.text) + '</span></div>';
 
 						} else {
-							return '<div class="svgicons__i"><div class="svgicons__i__w"><div class="svgicons__i__w__i"><img src="' + (item.value == '_blank_' ? options.blank : options.iconSetUrl + item.value) + '" alt="" /></div></div><span>' + escape(item.text) + '</span></div>';
+							return '<div class="svgicons__i"><div class="svgicons__i__w"><div class="svgicons__i__w__i"><img src="' + (item.value == '_blank_' ? self.options.blank : self.options.iconSetUrl + item.value) + '" alt="" /></div></div><span>' + escape(item.text) + '</span></div>';
 						}
 					}
 				}
 			}
 
 			$sel.selectize(opt)
-		},
-		loadSpriteSheets: function() {
-			var self = this;
-
-			for (var i = 0; i < this.options.spriteSheets.length; i++) {
-				var sheet = this.options.spriteSheets[i];
-				if ($.inArray(sheet, __svgicons.loaded) == -1) {
-					__svgicons.loaded.push(sheet)
-					$.get(sheet, function(data) {
-						var div = document.createElement('div')
-						div.innerHTML = new XMLSerializer().serializeToString(data.documentElement)
-						$svg = $(div).find('> svg')
-						$svg.css('display', 'none').prependTo('body')
-					});
-				}
-			}
 		}
 	});
 })(jQuery);
